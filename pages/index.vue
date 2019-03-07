ada1e346b4725ac7f26b3d37f39af9147a09cc12
jdj <jdj@elevisor.com>
2019-03-08 08:01:28 +0900

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My wondrous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+    <h1>Hello, I'm a first Vue/Nuxt app!</h1>
   </section>
 </template>
 
