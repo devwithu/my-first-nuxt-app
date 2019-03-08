ed400414b61cec2fa4a1f4ba0849f9cb7728601f
jdj <jdj@elevisor.com>
2019-03-08 16:12:18 +0900

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="container">
-    <h1>Hello, I'm a first Vue/Nuxt app!</h1>
-    <a href="/users"> Users</a>
+    <nuxt-link to="/users">Users</nuxt-link>
   </section>
 </template>
 
