65ecfeb9b5674da05078372d0fa53d2577fdc63b
jdj <jdj@elevisor.com>
2019-03-08 16:19:52 +0900

    .

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,3 +3,11 @@
     <h1>The single user view {{ $route.params.id}}</h1>
   
 </template>
+
+<script>
+export default {
+    validate(data) {
+        return /^\d+$/.test(data.params.id)
+    }
+}
+</script>
